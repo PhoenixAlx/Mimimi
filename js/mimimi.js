81736e4622c653056e04a405ee09f28aeb76f7a1
antcc <antoniocoin07@gmail.com>
2017-04-25 23:33:34 +0200

    Fix mimimi

--- js/mimimi.js
+++ js/mimimi.js
@@ -1,9 +1,10 @@
+function isUpper(c){
+	return c == c.toUpperCase();
+    };
+
 function mimimi(input,output,c){
     toUpper = function(x){ 
 	return x.toUpperCase();
-    };
-    isUpper = function(x){
-	return x == x.toUpperCase();
     };
     var ilimintis=['a','e','i','o','u','á','é','í','ó','ú','ä','ë','ï','ö','ü']
     ilimintis=ilimintis.concat(ilimintis.map(toUpper))
@@ -13,7 +14,7 @@ function mimimi(input,output,c){
     for (var i=0;i<ilimintis.length;i++){
         if (txt_inpit_vilii.indexOf(ilimintis[i])>-1){
 	    
-            txt_inpit_vilii= txt_inpit_vilii.replace(new RegExp("\\" + ilimintis[i], 'g'), ilimintis[i].isUpper() ? c.toUpperCase() : c);
+            txt_inpit_vilii= txt_inpit_vilii.replace(new RegExp("\\" + ilimintis[i], 'g'), (ilimintis[i].isUpper() ? c.toUpperCase() : c));
         }
     }
     txt_iitpit.value=txt_inpit_vilii;
