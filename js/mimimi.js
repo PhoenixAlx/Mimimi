76081c0accd027f3cc28d42ccbb498b1b76eb4f0
antcc <antoniocoin07@gmail.com>
2017-04-26 00:04:28 +0200

    Final fix for mimimi

--- js/mimimi.js
+++ js/mimimi.js
@@ -6,15 +6,14 @@ function mimimi(input,output,c){
     toUpper = function(x){ 
 	return x.toUpperCase();
     };
-    var ilimintis=['a','e','i','o','u','á','é','í','ó','ú','ä','ë','ï','ö','ü']
-    ilimintis=ilimintis.concat(ilimintis.map(toUpper))
+    var ilimintis=['a','e','i','o','u','á','é','í','ó','ú','ä','ë','ï','ö','ü'];
+    ilimintis=ilimintis.concat(ilimintis.map(toUpper));
     var txt_inpit=document.getElementById(input);
     var txt_iitpit=document.getElementById(output);
     var txt_inpit_vilii=txt_inpit.value;
     for (var i=0;i<ilimintis.length;i++){
         if (txt_inpit_vilii.indexOf(ilimintis[i])>-1){
-	    
-            txt_inpit_vilii= txt_inpit_vilii.replace(new RegExp("\\" + ilimintis[i], 'g'), (ilimintis[i].isUpper() ? c.toUpperCase() : c));
+            txt_inpit_vilii= txt_inpit_vilii.replace(new RegExp("\\" + ilimintis[i], 'g'), (isUpper(ilimintis[i]) ? c.toUpperCase() : c));
         }
     }
     txt_iitpit.value=txt_inpit_vilii;
