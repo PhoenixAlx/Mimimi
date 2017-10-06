93a9366b07523dbc74c4deed95d35646205b2079
josealberto4444 <jose.gif@hotmail.com>
2017-10-06 23:18:41 +0200

    Changing accents accordingly

--- js/mimimi.js
+++ js/mimimi.js
@@ -2,18 +2,23 @@ function isUpper(c){
     return c == c.toUpperCase();
 }
 
-function mimimi(input,output,c){
+function mimimi(input,output){
     toUpper = function(x){ 
-	return x.toUpperCase();
+        return x.toUpperCase();
     };
-    var ilimintis=['a','e','i','o','u','á','é','í','ó','ú','ä','ë','ï','ö','ü'];
-    ilimintis=ilimintis.concat(ilimintis.map(toUpper));
+    var ilimintis = [['a','e','o','u'], ['á','é','ó','ú'], ['ä','ë','ö','ü']];
+    var sistiticiinis = ['i','í','ï'];
+    for (var i = 0; i < ilimintis.length; i++) {
+        ilimintis[i] = ilimintis[i].concat(ilimintis[i].map(toUpper));
+    }
     var txt_inpit=document.getElementById(input);
     var txt_iitpit=document.getElementById(output);
     var txt_inpit_vilii=txt_inpit.value;
     for (var i=0;i<ilimintis.length;i++){
-        if (txt_inpit_vilii.indexOf(ilimintis[i])>-1){
-            txt_inpit_vilii= txt_inpit_vilii.replace(new RegExp("\\" + ilimintis[i], 'g'), (isUpper(ilimintis[i]) ? c.toUpperCase() : c));
+        for (var j = 0; j < ilimintis[i].length; j++) {
+            if (txt_inpit_vilii.indexOf(ilimintis[i][j])>-1){
+                txt_inpit_vilii= txt_inpit_vilii.replace(new RegExp("\\" + ilimintis[i][j], 'g'), (isUpper(ilimintis[i][j]) ? sistiticiinis[i].toUpperCase() : sistiticiinis[i]));
+            }
         }
     }
     txt_iitpit.value=txt_inpit_vilii;
